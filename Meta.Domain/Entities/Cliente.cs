d8681a92e7433fac85358996e152527483b9dae6
antonio silva <antonio@erpron.com.br>
2022-05-27 08:47:04 -0300

    Cliação de Classe Cliente

---
+++ Meta.Domain/Entities/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meta.Domain.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public int CPF { get; set; }
+        public string Nome { get; set; }
+    }
+}
